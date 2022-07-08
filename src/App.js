2ae7cb7658037ee6850276cef8e2966db0037ad5
Roman Drozdov <hamilion.91@mail.ru>
2022-07-08 14:11:30 +0300

    store and update tasks on the server

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import { useState, useEffect, useReducer } from "react";
-import './App.css'
-
-// fetch('http://localhost:5000/todos')
-// .then(response => response.json())
-// .then(data => console.log(data)
-//   )
+import { type } from "@testing-library/user-event/dist/type"
+import { useState, useEffect, useReducer } from "react"
+import "./App.css"
 
 const initialTasks = [
   { id: 1, text: "Learn React", done: true, date: new Date(), color: "green" },
-  { id: 2, text: "Learn Next js", done: false, date: new Date(), color: "red" }
-];
+  { id: 2, text: "Learn Next js", done: false, date: new Date(), color: "red" },
+]
 
-const availableColors = ["green", "blue", "orange", "purple", "red"];
+const availableColors = ["green", "blue", "orange", "purple", "red"]
 
 const StatusFilter = {
   ALL: "all",
   ACTIVE: "active",
-  COMPLETED: "completed"
-};
-
-function deserializeDates(tasks) {
-  return tasks.map((task) => {
-    task.date = new Date(task.date);
-    return task;
-  });
+  COMPLETED: "completed",
 }
 
-function useLocalStorage(key, initialValue, initializer = (arg) => arg) {
+// function deserializeDates(tasks) {
+//   return tasks.map(task => {
+//     task.date = new Date(task.date)
+//     return task
+//   })
+// }
+
+function useLocalStorage(key, initialValue, initializer = arg => arg) {
   const [state, setState] = useState(() => {
-    const storageValue = window.localStorage.getItem(key);
+    const storageValue = window.localStorage.getItem(key)
 
     if (storageValue !== null) {
       try {
-        return initializer(JSON.parse(storageValue));
+        return initializer(JSON.parse(storageValue))
       } catch (err) {
-        window.localStorage.removeItem(key);
+        window.localStorage.removeItem(key)
       }
     }
 
-    return initialValue;
-  });
+    return initialValue
+  })
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
 
-  return [state, setState];
+  return [state, setState]
 }
 
 function tasksReducer(currentTasks, action) {
@@ -58,38 +54,38 @@ function tasksReducer(currentTasks, action) {
           text: action.payload,
           done: false,
           date: new Date(),
-          color: ""
-        }
-      ];
+          color: "",
+        },
+      ]
     }
     case "deleteTask": {
-      return currentTasks.filter((task) => task.id !== action.payload);
+      return currentTasks.filter(task => task.id !== action.payload)
     }
     case "editTask": {
-      const { taskId, newText } = action.payload;
-      return currentTasks.map((task) =>
+      const { taskId, newText } = action.payload
+      return currentTasks.map(task =>
         task.id === taskId ? { ...task, text: newText } : task
-      );
+      )
     }
     case "toggleTask": {
-      return currentTasks.map((task) =>
+      return currentTasks.map(task =>
         task.id === action.payload ? { ...task, done: !task.done } : task
-      );
+      )
     }
     case "changeColor": {
-      const { taskId, newColor } = action.payload;
-      return currentTasks.map((task) =>
+      const { taskId, newColor } = action.payload
+      return currentTasks.map(task =>
         task.id === taskId ? { ...task, color: newColor } : task
-      );
+      )
     }
     case "markAllCompleted": {
-      return currentTasks.map((task) => ({ ...task, done: true }));
+      return currentTasks.map(task => ({ ...task, done: true }))
     }
     case "clearCompleted": {
-      return currentTasks.filter((task) => !task.done);
+      return currentTasks.filter(task => !task.done)
     }
     default: {
-      throw new Error(`Неожиданный action.type "${action.type}"`);
+      throw new Error(`Неожиданный action.type "${action.type}"`)
     }
   }
 }
@@ -98,105 +94,168 @@ function useLocalStorageReducer(
   key,
   reducer,
   initialValue,
-  initializer = (arg) => arg
+  initializer = arg => arg
 ) {
-  const [state, dispatch] = useReducer(
-    reducer,
-    initialValue,
-    (initialValue) => {
-      const loadFromLocalStorage = window.localStorage.getItem(key);
-      if (loadFromLocalStorage !== null) {
-        try {
-          return initializer(JSON.parse(loadFromLocalStorage));
-        } catch {
-          window.localStorage.removeItem(key);
-        }
+  const [state, dispatch] = useReducer(reducer, initialValue, initialValue => {
+    const loadFromLocalStorage = window.localStorage.getItem(key)
+    if (loadFromLocalStorage !== null) {
+      try {
+        return initializer(JSON.parse(loadFromLocalStorage))
+      } catch {
+        window.localStorage.removeItem(key)
       }
-      return initialValue;
     }
-  );
+    return initialValue
+  })
 
   useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    window.localStorage.setItem(key, JSON.stringify(state))
+  }, [key, state])
 
-  return [state, dispatch];
+  return [state, dispatch]
 }
 
 export default function App() {
+  // const [tasks, dispatch] = useLocalStorageReducer(
+  //   "tasks",
+  //   tasksReducer,
+  //   initialTasks,
+  // )
+  const [tasks, setTasks] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const dispatch = arg => arg
 
-  const [tasks, dispatch] = useLocalStorageReducer(
-    "tasks",
-    tasksReducer,
-    initialTasks,
-    deserializeDates
-  );
-  const [editedId, setEditedId] = useState(null);
+  // 1. Component Reusability (Shared State)
+  // 2. Server Side Rendering
+  // 3. New Features Incompatible
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      setIsLoading(true)
+      const response = await fetch("http://localhost:5000/todos")
+      const tasks = await response.json()
+      setTasks(tasks)
+      setIsLoading(false)
+    }
+    fetchTasks()
+  }, [])
+
+  const [editedId, setEditedId] = useState(null)
   const [statusFilter, setStatusFilter] = useLocalStorage(
     "statusFilter",
     StatusFilter.ALL
-  );
-  const [search, setSearch] = useLocalStorage("search", "");
-  const [colors, setColors] = useLocalStorage("colors", []);
+  )
+  const [search, setSearch] = useLocalStorage("search", "")
+  const [colors, setColors] = useLocalStorage("colors", [])
 
-  const cleanSearch = search.trim().toLowerCase();
-  let filteredTasks = tasks;
+  const cleanSearch = search.trim().toLowerCase()
+  let filteredTasks = tasks
 
   if (
     statusFilter !== StatusFilter.ALL ||
     colors.length !== 0 ||
     cleanSearch.length !== 0
   ) {
-    filteredTasks = tasks.filter((task) => {
-      const showDone = statusFilter === StatusFilter.COMPLETED;
+    filteredTasks = tasks.filter(task => {
+      const showDone = statusFilter === StatusFilter.COMPLETED
       const statusMatches =
-        statusFilter === StatusFilter.ALL || task.done === showDone;
-      const searchMatches = task.text.toLowerCase().includes(cleanSearch);
-      const colorMatches = colors.length === 0 || colors.includes(task.color);
-      return statusMatches && searchMatches && colorMatches;
-    });
+        statusFilter === StatusFilter.ALL || task.done === showDone
+      const searchMatches = task.text.toLowerCase().includes(cleanSearch)
+      const colorMatches = colors.length === 0 || colors.includes(task.color)
+      return statusMatches && searchMatches && colorMatches
+    })
   }
 
-  const handleAdd = (event) => {
-    event.preventDefault();
+  const handleAdd = async event => {
+    event.preventDefault()
 
-    const { value } = event.target.newTask;
-    const newText = value.trim();
+    const { value } = event.target.newTask
+    const newText = value.trim()
 
     if (newText === "") {
-      return;
+      return
     }
-    dispatch({ type: "addTask", payload: newText });
 
-    event.target.reset();
-    event.target.newTask.focus();
-  };
+    // dispatch({ type: "addTask", payload: newText })
+    const response = await fetch("http://localhost:5000/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: newText,
+        done: false,
+        date: new Date(),
+        color: "",
+      }),
+    })
+    const newTask = await response.json()
+    setTasks(prevTasks => [...prevTasks, newTask])
+
+    event.target.reset()
+    event.target.newTask.focus()
+  }
 
   const handleEdit = (event, taskId) => {
-    event.preventDefault();
+    event.preventDefault()
 
-    const { value } = event.target.changeName;
-    const newText = value.trim();
+    const { value } = event.target.changeName
+    const newText = value.trim()
 
     if (newText === "") {
-      return;
+      return
     }
 
-    dispatch({ type: "editTask", payload: { taskId, newText } });
-    setEditedId(null);
-  };
+    // dispatch({ type: "editTask", payload: { taskId, newText } })
+    updateTask(taskId, { text: newText })
+    setEditedId(null)
+  }
+
+  const handleDelete = async taskId => {
+    // dispatch({ type: "deleteTask", payload: taskId })
+    const response = await fetch(`http://localhost:5000/todos/${taskId}`, {
+      method: "DELETE",
+    })
+    if (response.ok) {
+      setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId))
+    }
+  }
+
+  const handleToggle = task => {
+    // dispatch({ type: "toggleTask", payload: taskId })
+    updateTask(task.id, { done: !task.done })
+  }
+
+  const handleChangeColor = (taskId, newColor) => {
+    // dispatch({ type: 'changeColor', payload: { taskId, newColor }})
+    updateTask(taskId, { color: newColor })
+  }
+
+  const updateTask = async (taskId, data) => {
+    const response = await fetch(`http://localhost:5000/todos/${taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    const updatedTask = await response.json()
+    setTasks(prevTasks =>
+      prevTasks.map(t => (t.id === taskId ? updatedTask : t))
+    )
+  }
 
   const { activeTasks, completedTasks } = tasks.reduce(
     (obj, task) => {
       if (task.done) {
-        obj.completedTasks++;
+        obj.completedTasks++
       } else {
-        obj.activeTasks++;
+        obj.activeTasks++
       }
-      return obj;
+      return obj
     },
     { activeTasks: 0, completedTasks: 0 }
-  );
+  )
 
   return (
     <div className="App">
@@ -212,12 +271,12 @@ export default function App() {
             <button
               onClick={() => setStatusFilter(statusValue)}
               style={{
-                fontWeight: statusFilter === statusValue ? "bold" : null
+                fontWeight: statusFilter === statusValue ? "bold" : null,
               }}
             >
               {capitalize(statusName.toLowerCase())}
             </button>
-          );
+          )
         })}
       </div>
 
@@ -230,74 +289,75 @@ export default function App() {
         type="search"
         placeholder="Search..."
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={e => setSearch(e.target.value)}
       />
+      {isLoading ? (
+        <p>Is Loading...</p>
+      ) : (
+        <ul style={{ listStyle: "none" }}>
+          {filteredTasks.map(task => {
+            const taskDate = new Date(task.date)
+            return (
+              <li>
+                {editedId === task.id ? (
+                  <form onSubmit={e => handleEdit(e, task.id)}>
+                    <input
+                      type="text"
+                      name="changeName"
+                      defaultValue={task.text}
+                      autoFocus
+                    />
+                    <button type="submit">Save</button>
+                    <button type="button" onClick={() => setEditedId(null)}>
+                      Cancel
+                    </button>
+                  </form>
+                ) : (
+                  <div>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={task.done}
+                        onChange={() => handleToggle(task)}
+                      />
+                      {task.done ? <del>{task.text}</del> : task.text}
+                    </label>
+                    <select
+                      style={{ color: task.color }}
+                      value={task.color}
+                      onChange={e =>
+                        // dispatch({
+                        //   type: "changeColor",
+                        //   payload: { taskId: task.id, newColor: e.target.value },
+                        // })
+                        handleChangeColor(task.id, e.target.value)
+                      }
+                    >
+                      <option></option>
+                      {availableColors.map(color => {
+                        return (
+                          <option value={color} style={{ color }}>
+                            {capitalize(color)}
+                          </option>
+                        )
+                      })}
+                    </select>
+                    <button onClick={() => setEditedId(task.id)}>Edit</button>
+                    <button onClick={() => handleDelete(task.id)}>
+                      Delete
+                    </button>
 
-      <ul style={{ listStyle: "none" }}>
-        {filteredTasks.map((task) => (
-          <li>
-            {editedId === task.id ? (
-              <form onSubmit={(e) => handleEdit(e, task.id)}>
-                <input
-                  type="text"
-                  name="changeName"
-                  defaultValue={task.text}
-                  autoFocus
-                />
-                <button type="submit">Save</button>
-                <button type="button" onClick={() => setEditedId(null)}>
-                  Cancel
-                </button>
-              </form>
-            ) : (
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={task.done}
-                    onChange={() =>
-                      dispatch({ type: "toggleTask", payload: task.id })
-                    }
-                  />
-                  {task.done ? <del>{task.text}</del> : task.text}
-                </label>
-                <select
-                  style={{ color: task.color }}
-                  value={task.color}
-                  onChange={(e) =>
-                    dispatch({
-                      type: "changeColor",
-                      payload: { taskId: task.id, newColor: e.target.value }
-                    })
-                  }
-                >
-                  <option></option>
-                  {availableColors.map((color) => {
-                    return (
-                      <option value={color} style={{ color }}>
-                        {capitalize(color)}
-                      </option>
-                    );
-                  })}
-                </select>
-                <button onClick={() => setEditedId(task.id)}>Edit</button>
-                <button
-                  onClick={() =>
-                    dispatch({ type: "deleteTask", payload: task.id })
-                  }
-                >
-                  Delete
-                </button>
-
-                <span>
-                  {task.date.toLocaleTimeString(["ru"])},{" "}
-                  {task.date.toLocaleDateString(["ru"])}
-                </span>
-              </div>
-            )}
-          </li>
-        ))}
-      </ul>
+                    <span>
+                      {taskDate.toLocaleTimeString(["ru"])},{" "}
+                      {taskDate.toLocaleDateString(["ru"])}
+                    </span>
+                  </div>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      )}
       <p>
         {`Total: ${tasks.length}, Active: ${activeTasks}, Completed: ${completedTasks}`}
       </p>
@@ -312,12 +372,12 @@ export default function App() {
 
       <div>
         <h2>Filter By Color</h2>
-        {availableColors.map((color) => {
+        {availableColors.map(color => {
           function handleCheckboxChange(e) {
             if (e.target.checked) {
-              setColors((prevColors) => [...prevColors, color]);
+              setColors(prevColors => [...prevColors, color])
             } else {
-              setColors((prevColors) => prevColors.filter((c) => c !== color));
+              setColors(prevColors => prevColors.filter(c => c !== color))
             }
           }
           return (
@@ -329,13 +389,15 @@ export default function App() {
               />
               {capitalize(color)}
             </label>
-          );
+          )
         })}
       </div>
     </div>
-  );
+  )
 }
 
 function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1);
+  return str[0].toUpperCase() + str.slice(1)
 }
+
+// fetch("http://localhost:5000/todos/4", { method: "DELETE" })
